cc48ffd595cd8365e86e62d2bebe456b15d57c95
Gabriel Tavares <gsantostavares@gmail.com>
2024-09-02 23:24:23 -0300

    DataFrame has been done

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from geopy.geocoders import Nominatim
-
-geolocator = Nominatim(user_agent= 'app.py')
-location = geolocator.geocode("Rua João Balbino, 10, Uberlândia, MG")
-print(location.latitude, location.longitude)
-
 import sqlite3 
+from geopy.geocoders import Nominatim
+import pandas as pd
 
 connection_obj = sqlite3.connect('database.db') 
-
 cursor_obj = connection_obj.cursor() 
 
 cursor_obj.execute( 
@@ -19,7 +14,22 @@ inner join address a
 on u.user_id = a.user_id
 """
 ) 
-
-query = cursor_obj.fetchall()
-#print(query)
+users = cursor_obj.fetchall()
 connection_obj.close() 
+
+geolocator = Nominatim(user_agent= 'app.py', timeout= 10)
+data_to_df = []
+for user in users:
+    user = list(user)
+    location = geolocator.geocode(user[2])
+    try:
+        user.append(location.latitude)
+        user.append(location.longitude)
+    except:
+        user.append(None)
+        user.append(None)
+    data_to_df.append(user)
+
+df = pd.DataFrame(data=data_to_df, 
+                  columns=['user_name', 'user_type_name', 
+                           'address_description', 'latitude', 'longitude'])
