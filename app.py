2e64edf55f82c931fccfd46003c8c1bca83bbcd9
Gabriel Tavares <gsantostavares@gmail.com>
2024-09-03 23:16:40 -0300

    alteração da fonte de dados de consulta sql para dado mockado

--- app.py
+++ app.py
@@ -1,35 +1,28 @@
-import sqlite3 
 from geopy.geocoders import Nominatim
 import pandas as pd
 
-connection_obj = sqlite3.connect('database.db') 
-cursor_obj = connection_obj.cursor() 
-
-cursor_obj.execute( 
-"""SELECT u.user_name, ut.user_type_name, a.address_description 
-from user u
-inner join user_type ut 
-on u.user_id = ut.user_id
-inner join address a 
-on u.user_id = a.user_id
-"""
-) 
-users = cursor_obj.fetchall()
-connection_obj.close() 
+users = [
+    {'nome': 'Ana', 'tipo_cliente': 'Casual', 'endereco': 'Avenida Vasconcelos Costa, 270, Uberlândia, MG'},
+    {'nome': 'Maria', 'tipo_cliente': 'Casual', 'endereco': 'Avenida Presidente Médici, 1001, Uberlândia, MG'},
+    {'nome': 'Fernanda', 'tipo_cliente': 'Casual', 'endereco': 'Avenida Getúlio Vargas, 450, Uberlândia, MG'},
+    {'nome': 'Vanessa', 'tipo_cliente': 'Frequente', 'endereco': 'Avenida João Naves de Ávila, 999, Uberlândia, MG'},
+    {'nome': 'Bia', 'tipo_cliente': 'Frequente', 'endereco': 'Avenida Seme Simão, 999, Uberlândia, MG'}
+]
 
 geolocator = Nominatim(user_agent= 'app.py', timeout= 10)
 data_to_df = []
 for user in users:
-    user = list(user)
-    location = geolocator.geocode(user[2])
+    
+    location = geolocator.geocode(user['endereco'])
+    
     try:
-        user.append(location.latitude)
-        user.append(location.longitude)
+        user['latitude'] = location.latitude
+        user['longitude'] = location.longitude
     except:
-        user.append(None)
-        user.append(None)
+        user['latitude'] = None
+        user['longitude'] = None
     data_to_df.append(user)
 
 df = pd.DataFrame(data=data_to_df, 
-                  columns=['user_name', 'user_type_name', 
-                           'address_description', 'latitude', 'longitude'])
+                  columns=['nome', 'tipo_cliente', 
+                           'endereco', 'latitude', 'longitude'])
